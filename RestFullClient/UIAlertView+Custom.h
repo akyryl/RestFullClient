12562f30bf7402f18d030def75e0006c50122658
akyryl <akyryl@softserveinc.com>
2014-03-24 21:48:04 +0200

    Added connection error alerting

---
+++ RestFullClient/UIAlertView+Custom.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+Custom.h
+//  RestFullClient
+//
+//  Created by akyryl on 3/24/14.
+//  Copyright (c) 2014 Anatolii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView_Custom : UIAlertView
+
+@end
---
+++ RestFullClient/UIAlertView+Custom.m
@@ -0,0 +1,31 @@
+//
+//  UIAlertView+Custom.m
+//  RestFullClient
+//
+//  Created by akyryl on 3/24/14.
+//  Copyright (c) 2014 Anatolii. All rights reserved.
+//
+
+#import "UIAlertView+Custom.h"
+
+@implementation UIAlertView_Custom
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
